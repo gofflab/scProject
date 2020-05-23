2eb988634b49261bfe59e01358fd9777df4017e7
ABaraban <54551616+ABaraban@users.noreply.github.com>
2020-05-23 12:19:53 -0400

    Add files via upload

---
+++ pyproject/LSHMethods.py
@@ -0,0 +1,39 @@
+# What kind of methods are necessary for LSH
+# Also, this must extend anndata objects so it
+# can interface with scanpy
+import numpy as np
+import scanpy as sc
+
+
+# param table-the hashtable
+# param vector- the vector being inserted
+# param random_vector-the random_vector that we are projecting onto for that round of hashing
+# param bucket_width-the sensitivity
+# return it is void but it inserts the vector into the hashtable
+def insertToTable(table, vector, random_vector, bucket_width):
+    hash_index = np.floor(np.dot(vector, random_vector) / bucket_width)
+    table[hash_index].append(vector)
+    return hash_index % len(table)
+
+
+# This method hashes the new dataset and the library
+# param file-the name of the anndata object holding the new dataset
+# param buckets-the size of the hashtable
+# param library_file name of the anndata object holding the library file
+# returns a hashtable(w/ chaining) and which buckets contain the library
+def hash_entire_dataset(file, buckets, library_file):
+    ad = sc.read(file)
+    library = sc.read(library_file)
+    random_Hash_Vector = np.random.rand(1, ad.n_obs)
+    hashtable = [[] for _ in range(buckets)]
+    library_indices = []
+
+    for i in range(ad.n_vars):
+        vec = ad.var_vector(i)
+        insertToTable(hashtable, vec, random_Hash_Vector)
+    for i in range(library.n_vars):
+        vec_lib = library.var_vector(i)
+        index = insertToTable(hashtable, vec_lib, random_Hash_Vector)
+        library_indices.append(index)
+
+    return hashtable, library_indices
---
+++ pyproject/LSH_Object.py
@@ -0,0 +1,63 @@
+import scanpy as sc
+import numpy as np
+from pyproject.LSHMethods import insertToTable
+
+
+class Hashing:
+    def __init__(self, bucket_width, num_buckets, library, file, hash_vectors):
+        self.bucket_width = bucket_width
+        self.library = sc.read(library)
+        self.file = sc.read(file)
+        self.num_buckets = num_buckets
+        self.hashtable = [[] for _ in range(num_buckets)]
+        self.random_vector = None
+        self.hash_vectors = sc.read(hash_vectors)
+
+    def hash_LSH_Random_Vectors(self):
+        self.random_vector = np.random.rand(1, self.file.n_obs)
+        hashtable = [[] for _ in range(self.num_buckets)]
+        library_indices = []
+        for i in range(self.library.n_vars):
+            vec_lib = self.library.var_vector(i)
+            index = insertToTable(self.hashtable, vec_lib, self.random_vector, self.bucket_width)
+            library_indices.append(index)
+        for i in range(self.file.n_vars):
+            vec = self.file.var_vector(i)
+            insertToTable(self.hashtable, vec, self.random_vector, self.bucket_width)
+        return hashtable, library_indices
+
+    def hash_LSH_Chosen_Vector(self, hash_vector):
+        hashtable = [[] for _ in range(self.num_buckets)]
+        library_indices = []
+        for i in range(self.library.n_vars):
+            vec_lib = self.library.var_vector(i)
+            index = insertToTable(self.hashtable, vec_lib, hash_vector, self.bucket_width)
+            library_indices.append(index)
+        for i in range(self.file.n_vars):
+            vec = self.file.var_vector(i)
+            insertToTable(self.hashtable, vec, hash_vector, self.bucket_width)
+
+        return hashtable, library_indices
+
+    def hash_multiple_times_Chosen(self, number_of_times):
+        list_of_tables = []
+        if number_of_times > self.hash_vectors.n_vars:
+            number_of_times = self.hash_vectors.n_vars
+        for i in range(number_of_times):
+            hash_vector = self.hash_vectors.var_vector(i)
+            list_of_tables.append(self.hash_LSH_Chosen_Vector(hash_vector))
+        return list_of_tables
+
+# I'm having some trouble figuring out how to do the intersection
+    def hash_multiple_times_random(self, number_of_times):
+        list_of_tables = []
+        list_of_clusters = []
+        for i in range(number_of_times):
+            list_of_tables.append(self.hash_LSH_Random_Vectors())
+        for i in range(len(list_of_tables[0][1])):# is that correct?
+            # I do not know to take this intersection here
+
+        return list_of_clusters #the clusters with library vectors
+
+
+
