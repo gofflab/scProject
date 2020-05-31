7a5f70b71ea6495e5cb0249d70313188fb35d981
asherbaraban <asher.baraban@wustl.edu>
2020-05-31 16:29:17 -0400

    Debugged

---
+++ pyproject/test_LSH/LSH_Tests.py
@@ -0,0 +1,18 @@
+import unittest
+import pyproject
+from pyproject.LSH_Object import Hashing
+
+
+def main():
+    hasher = Hashing(5, 571, "retina_patterns.csv", "retina_patterns.csv")
+    hasher2 = Hashing(5, 571, "retina_patterns.csv", "retina_patterns.csv", "retina_patterns.csv")
+    print("fin constructor")
+    list_clusters = hasher.hash_multiple_times_random(3)
+    list_clusters = hasher2.hash_multiple_times_Chosen(3)
+    print("done random")
+
+
+if __name__ == '__main__':
+    main()
+    print("woohoo")
+
