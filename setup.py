887dc34f07768728e92d55dbfe8b52b96558c30e
asherbaraban <asher.baraban@wustl.edu>
2020-08-20 18:56:03 -0400

    It builds on test pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 from setuptools import setup
+import setuptools
 
 setup(
+    packages=setuptools.find_packages(),
     name='ProjectPy',
-    version='0.1.3',
+    version='0.1.4',
     author='Asher Baraban, Loyal Goff',
     author_email='loyalgoff@jhmi.edu',
-    packages=['ProjectPy', 'ProjectPy.test'],
     scripts=[],
     url="https://github.com/gofflab/ProjectPy.git",
     license='MIT',
     description='Transfer learning framework for single cell gene expression analysis in Python',
     long_description=open('README.md').read(),
-    install_requires=[
-        "anndata >= 0.7",  #
-        "numpy >= 1.18.1",  #
-        "pandas >= 1.0.1",
-        "scipy >= 1.4.1",
-        "seaborn >= 0.10.0",  #
-        "matplotlib >= 3.1.3",
-        "umap-learn >=0.4.0",
-        "scikit-learn>=0.21.2",
-    ],
 )
