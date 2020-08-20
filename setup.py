2b2aec247c7baf9c1abb31ca261ad56a3fec57ba
asherbaraban <asher.baraban@wustl.edu>
2020-08-20 15:29:13 -0400

    tweaked requirements again sklearn is giving me trouble

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ProjectPy',
-    version='0.1.0',
+    version='0.1.1',
     author='Asher Baraban, Loyal Goff',
     author_email='loyalgoff@jhmi.edu',
     packages=['ProjectPy', 'ProjectPy.test'],
