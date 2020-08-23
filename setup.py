53f29c361f9f07cc8a21835b4998b9564e994fe2
asherbaraban <asher.baraban@wustl.edu>
2020-08-23 10:25:29 -0400

    Real pip!!!

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setup(
     packages=setuptools.find_packages(),
     name='ProjectPy',
-    version='0.1.6',
+    version='1.0.0',
     author='Asher Baraban, Loyal Goff',
     author_email='loyalgoff@jhmi.edu',
     scripts=[],
@@ -12,4 +12,7 @@ setup(
     license='MIT',
     description='Transfer learning framework for single cell gene expression analysis in Python',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ]
 )
