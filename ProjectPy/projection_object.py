b541fe6de1e3f1b65df0fdcf94d54a9d040c52ba
asherbaraban <asher.baraban@wustl.edu>
2020-08-22 22:07:06 -0400

    Added Legend to UMAP, which I should have done a while ago.

--- ProjectPy/projection_object.py
+++ ProjectPy/projection_object.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import seaborn as sns
 import umap
@@ -6,69 +7,6 @@ from scipy import stats
 from scipy.optimize import nnls
 from sklearn import linear_model
 from . import matcher
-
-
-# Super Class- Not in use all refactored below
-class projection:
-    def __init__(self, dataset, patterns, cellTypeColumnName, genecolumnname, num_cell_types):
-        self.dataset = dataset
-        self.patterns = patterns
-        dataset.var = dataset.var.set_index(genecolumnname)
-        overlap = dataset.var.index.intersection(patterns.var.index)
-        self.dataset_filtered = dataset[:, overlap]
-        print(self.dataset_filtered.shape, "dataset filter shape")
-        self.patterns_filtered = patterns[:, overlap]
-        print(self.patterns_filtered.shape, "patterns filter shape")
-        self.cellTypeColumnName = cellTypeColumnName
-        self.model = None
-        self.pearsonMatrix = None
-        self.num_cell_types = num_cell_types
-        self.num_patterns = patterns.X.shape[0]
-        self.UMAP_COORD = None
-
-    def non_neg_lin_reg(self, alpha, L1, iterations=10000):
-        model = linear_model.ElasticNet(alpha=alpha, l1_ratio=L1, max_iter=iterations)
-        model.fit(self.patterns_filtered.X.T, self.dataset_filtered.X.T)
-        self.model = model
-
-    def pearsonPlot(self, plot=True):
-        color = matcher.mapCellNamesToInts(self.dataset_filtered, self.cellTypeColumnName)
-        matrix = np.zeros([self.num_cell_types, color.shape[0]])
-        pearson_matrix = np.empty([self.patterns_filtered.X.shape[0], self.num_cell_types])
-        for i in range(color.shape[0]):
-            cell_type = color[i]
-            matrix[cell_type][i] = 1
-        for i in range(self.patterns_filtered.X.shape[0]):
-            pattern = np.transpose(self.model.coef_)[:][i]
-            for j in range(color.unique().shape[0]):
-                cell_type = matrix[j]
-                correlation = stats.pearsonr(pattern, cell_type)
-                pearson_matrix[i][j] = correlation[0]
-        self.pearsonMatrix = pearson_matrix
-        if plot:
-            plt.title("Pearson Plot", fontsize=24)
-            graphic = sns.heatmap(pearson_matrix)
-            plt.show()
-
-    def UMAP_Projection(self, n_neighbors=10, metric='euclidean', plot=True, color="Paired"):
-        color = matcher.mapCellNamesToInts(self.dataset_filtered, self.cellTypeColumnName)
-        umap_obj = umap.UMAP(n_neighbors=n_neighbors, metric=metric)
-        nd = umap_obj.fit_transform(self.model.coef_)
-        if plot:
-            plt.scatter(nd[:, 0], nd[:, 1],
-                        c=[sns.color_palette("Paired", n_colors=12)[x] for x in color], s=.5)
-            plt.title("UMAP Projection of Pattern Matrix", fontsize=24)
-            plt.show()
-        self.UMAP_COORD = nd
-
-    def featurePlots(self):
-        for i in range(self.num_patterns):
-            feature = self.model.coef_[:, i]
-            plt.title("Feature " + str(i + 1), fontsize=24)
-            plt.scatter(self.UMAP_COORD[:, 0], self.UMAP_COORD[:, 1], c=feature, cmap='jet', s=.5)
-            plt.colorbar()
-            print(np.count_nonzero(feature))
-            plt.show()
 
 
 def filterAnnDatas(dataset, patterns, geneColumnName):
@@ -194,7 +132,7 @@ def pearsonMatrix(dataset_filtered, patterns_filtered, cellTypeColumnName, num_c
 
 
 def pearsonViz(dataset_filtered, plotName):
-    """ Visualize a Pearson Matrix
+    """ Visualize a Pearson Matrix.
 
     :param dataset_filtered: Anndata object cells x genes
     :param plotName: Index of pearson matrix to visualize
@@ -212,8 +150,9 @@ def pearsonViz(dataset_filtered, plotName):
 # higher dimensions. Maybe, trying some other metrics could be interesting.)
 
 def UMAP_Projection(dataset_filtered, cellTypeColumnName, projectionName, UMAPName, n_neighbors, metric='euclidean',
-                    plot=True, color='Paired', pointSize=.5):
-    """
+                    plot=True, colorScheme='Paired', pointSize=.5):
+    """This method projects the pattern matrix down into 2 dimensional space. Make sure the colorScheme chosen has
+    enough colors for every cell type.
 
     :param dataset_filtered: Anndata object cells x genes
     :param cellTypeColumnName: index for cell type in dataset_filtered.obsm
@@ -222,41 +161,49 @@ def UMAP_Projection(dataset_filtered, cellTypeColumnName, projectionName, UMAPNa
     :param n_neighbors: number of neighbors for the UMAP
     :param metric: the distance metric used in the UMAP, defaults to euclidean
     :param plot: If True a plot is displayed, defaults to True
-    :param color: seaborn color scheme to use, defaults to Paired
+    :param colorScheme: seaborn color scheme to use, defaults to Paired
     :param pointSize: size of the points, defaults to .5
     :return: void, mutates dataset_filtered and add the UMAP to obsm
     """
     matcher.sourceIsValid(dataset_filtered)
-    color = matcher.mapCellNamesToInts(dataset_filtered, cellTypeColumnName)
     umap_obj = umap.UMAP(n_neighbors=n_neighbors, metric=metric)
     nd = umap_obj.fit_transform(dataset_filtered.obsm[projectionName])
     dataset_filtered.obsm[UMAPName] = nd
     if plot:
-        UMAP_Viz(dataset_filtered, UMAPName, color, pointSize)
+        UMAP_Viz(dataset_filtered, UMAPName, cellTypeColumnName, colorScheme, pointSize)
 
 
-def UMAP_Viz(dataset_filtered, UMAPName, color='Paired', pointSize=.5):
-    """
+def UMAP_Viz(dataset_filtered, UMAPName, cellTypeColumnName, colorScheme='Paired', pointSize=.5):
+    """Plots the UMAP of the pattern matrix. Make sure colorScheme has at least as many colors as cell types in your
+    dataset.
 
     :param dataset_filtered: Anndata object cells x genes
     :param UMAPName: index for the UMAP in dataset_filtered.obsm
-    :param color: seaborn color scheme, defaults to Paired
+    :param colorScheme: seaborn color scheme, defaults to Paired
     :param pointSize: size of the points, defaults to .5
     :return: void
     """
     matcher.sourceIsValid(dataset_filtered)
+    color = matcher.mapCellNamesToInts(dataset_filtered, cellTypeColumnName)
     nd = dataset_filtered.obsm[UMAPName]
     plt.scatter(nd[:, 0], nd[:, 1],
-                c=[sns.color_palette("Paired", n_colors=12)[x] for x in color], s=pointSize)
+                c=[sns.color_palette(colorScheme,
+                n_colors=dataset_filtered.obs[cellTypeColumnName].unique().shape[0])[x] for x in color], s=pointSize)
     plt.title("UMAP Projection of Pattern Matrix", fontsize=24)
+    handles = []
+    for i in range(dataset_filtered.obs[cellTypeColumnName].unique().shape[0]):
+        handles.append(mpatches.Patch(color=(sns.color_palette(colorScheme, n_colors=12)[i]),
+                                      label=dataset_filtered.obs[cellTypeColumnName].unique()[i]))
+    plt.figlegend(handles=handles, title="Cell Types", fontsize='x-small', loc=4)
     plt.show()
 
 
 def featurePlots(dataset_filtered, num_patterns, projectionName, UMAPName):
-    """Creates plots which show the weight of each feature in each cell
+    """Creates plots which show the weight of each feature in each cell.
 
     :param dataset_filtered: Anndata object cells x genes
-    :param num_patterns: the number of the patterns (this could be removed)
+    :param num_patterns: the number of the patterns. This can also take in a list of ints corresponding to features of
+     interest.
     :param projectionName: index of the projection in dataset_filtered.obsm
     :param UMAPName: index of the UMAP in dataset_filtered.obsm
     :return: void
